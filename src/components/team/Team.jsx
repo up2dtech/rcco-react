1bc3e7a1882454f9efba19b08388e31b798015b2
Saleha Qamar <salehaqamar7@gmail.com>
2023-09-07 00:37:31 -0700

    team design changes

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -6,7 +6,9 @@ import TeamImg2 from "../../assets/Images/Umer.jpeg";
 import TeamImg3 from "../../assets/Images/asif.jpeg";
 import TeamImg4 from "../../assets/Images/mehwish.jpg";
 import TeamImg5 from "../../assets/Images/ruhan.jpeg";
-import TeamImg6 from "../../assets/Images/bob.png";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Row, Col } from "react-bootstrap";
 
 export default function Team() {
   return (
@@ -25,7 +27,7 @@ export default function Team() {
         <div className="row">    
              
           {/* ======== */}
-          <div className="col-lg-4 col-md-4 col-sm-12 d-flex align-items-stretch tcol">
+          {/* <div className="col-lg-4 col-md-4 col-sm-12 d-flex align-items-stretch tcol">
             <div className="member">
               <div className="member-img">    
                 <img className="img-fluid" src={TeamImg1}  alt="Riaz"/>
@@ -50,151 +52,92 @@ export default function Team() {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
+
+         <Col md={4} lg={4}>
+           <Card style={{ width: '100%', height:'550px'  }}>
+            <Card.Img variant="top" src={TeamImg1}  alt="Riaz" height={400}/>
+             <Card.Body>
+             <Card.Title className="title">RIAZ AHMED CHISHTI</Card.Title>
+               <Card.Text className="teaminfo">
+                <span className="tspan">EXPERT INVESTMENT ADVISORY</span><br/>
+                   Founding Member, Ex.Vice President Habib Bank Ltd
+              </Card.Text>       
+             </Card.Body>
+             </Card>
+           </Col>
           {/* ======== */}
-          <div className="col-lg-4 col-md-4 col-sm-12 d-flex align-items-stretch tcol">
-            <div className="member">
-              <div className="member-img">
-              <img className="img-fluid" src={TeamImg2}  alt="Umer"/>
-              </div>
-              <div className="member-info">
-                <h5>MUHAMMAD UMER RIAZ</h5>
-                <span>MANAGING PARTNER</span>
-                <p>ACCA Member, M.com,Bsc</p>
-              </div>
-              <div className="social">
-                <a href="">
-                <FontAwesomeIcon icon={faTwitter} className="fa-1x" />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faInstagram} className="fa-1x" />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faLinkedin} className="fa-1x" />
-                </a>
-              </div>
-            </div>
-          </div>
+          <Col md={4} lg={4}>
+           <Card style={{ width: '100%', height:'550px' }}>
+            <Card.Img variant="top" src={TeamImg2}  alt="Riaz" height={400}/>
+             <Card.Body>
+             <Card.Title className="title">MUHAMMAD UMER RIAZ</Card.Title>
+               <Card.Text className="teaminfo">
+                <span className="tspan">MANAGING PARTNER</span><br/>
+                ACCA Member, M.com,Bsc
+              </Card.Text>       
+             </Card.Body>
+             </Card>
+           </Col>
           {/* ======== */}
-          <div className="col-lg-4 col-md-4 col-sm-12 d-flex align-items-stretch tcol">
-            <div className="member">
-              <div className="member-img">
-              <img className="img-fluid" src={TeamImg3}  alt="Asif"/>
-              </div>
-              <div className="member-info">
-                <h5>DR. ASIF CHISHTI</h5>
-                <span>DIRECTOR FINANCIAL ADVISORY</span>
-                <p>
-                  MD, Master in Management & Leadership (Harvard University),
-                  Master in Policy & Development (London School of Economics){" "}
-                </p>
-              </div>
-              <div className="social">
-                <a href="">
-                <FontAwesomeIcon icon={faTwitter} className="fa-1x" />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faInstagram} className="fa-1x" />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faLinkedin} className="fa-1x" />
-                </a>
-              </div>
-            </div>
-          </div>
+          <Col md={4} lg={4}>
+           <Card style={{ width: '100%', height:'550px' }}>
+            <Card.Img variant="top" src={TeamImg3}  alt="Riaz" height={400}/>
+             <Card.Body>
+             <Card.Title className="title">DR. ASIF CHISHTI</Card.Title>
+               <Card.Text className="teaminfo">
+                <span className="tspan">DIRECTOR FINANCIAL ADVISORY</span><br/>
+                MD, Master in Management & Leadership (Harvard University),
+                  Master in Policy & Development (London School of Economics)
+              </Card.Text>       
+             </Card.Body>
+             </Card>
+           </Col>
           {/* ======== */}
-          </div>
+          </div> 
           {/* =======end row1==== */}
        
           {/* ====2nd row=== */}
-          <div class="row">
+          <div class="row mt-3">
           
-     <div class="col-lg-4 col-md-4 col-sm-12 tcol">
-        <div class="member">
-          <div class="member-img">
-          <img className="img-fluid" src={TeamImg4}  alt="Mehwish"/>
-           </div>
-          <div class="member-info">
-            <h5>MEHVISH MASOUD</h5>
-            <span>MANAGER AUDIT & ASSURANCE</span>
-            <p>CA(finalist), Bsc</p>
-          </div>
-          <div className="social">
-                <a href="">
-                <FontAwesomeIcon icon={faTwitter} className="fa-1x" />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faInstagram} className="fa-1x" />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faLinkedin} className="fa-1x" />
-                </a>
-              </div>
-        </div>
-      </div>
+          <Col md={4} lg={4}>
+           <Card style={{ width: '100%' , height:'550px' }}>
+            <Card.Img variant="top" src={TeamImg4}  alt="Riaz" height={400}/>
+             <Card.Body>
+             <Card.Title className="title">MEHVISH MASOUD</Card.Title>
+               <Card.Text className="teaminfo">
+                <span className="tspan">MANAGER AUDIT & ASSURANCE</span><br/>
+                CA(finalist), Bsc
+              </Card.Text>       
+             </Card.Body>
+             </Card>
+           </Col>
         {/* ======== */}
-      <div class="col-lg-4 col-md-4 col-sm-12 d-flex align-items-stretch tcol">
-        <div class="member">
-          <div class="member-img">
-          <img className="img-fluid" src={TeamImg5}  alt="Ruhan"/>
-          </div>
-          <div class="member-info">
-            <h5>MUHAMMAD RUHAN AFZAL</h5>
-            <span>SPECIALIST LEGAL & COPORATE AFFAIRS</span>
-            <p>Advocate High Court</p>
-          </div>
-          <div className="social">
-                <a href="">
-                <FontAwesomeIcon icon={faTwitter} className="fa-1x" />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faInstagram} className="fa-1x" />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faLinkedin} className="fa-1x" />
-                </a>
-              </div>
-        </div>
-      </div>
+        <Col md={4} lg={4}>
+           <Card style={{ width: '100%' , height:'550px' }}>
+            <Card.Img variant="top" src={TeamImg5}  alt="Riaz" height={400}/>
+             <Card.Body>
+             <Card.Title className="title">MUHAMMAD RUHAN AFZAL</Card.Title>
+               <Card.Text className="teaminfo">
+                <span className="tspan">SPECIALIST LEGAL & COPORATE AFFAIRS</span><br/>
+                Advocate High Court
+              </Card.Text>       
+             </Card.Body>
+             </Card>
+           </Col>
       {/* ======== */}
-      <div class="col-lg-4 col-md-4 col-sm-12 d-flex align-items-stretch tcol">
-        <div class="member">
-          <div class="member-img">
-          <img className="img-fluid" src={TeamImg4}  alt="Abbas"/>
-          </div>
-          <div class="member-info">
-            <h5>RABIA RIAZ</h5>
-            <span>HR & IT SPECIALIST</span>
-            <p>Experienced HR Manager from the Aviation Industry</p>
-          </div>
-          <div className="social">
-                <a href="">
-                <FontAwesomeIcon icon={faTwitter} className="fa-1x" />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faInstagram} className="fa-1x" />
-                </a>
-                <a href="">
-                <FontAwesomeIcon icon={faLinkedin} className="fa-1x" />
-                </a>
-              </div>
-        </div>
-      </div>
+      <Col md={4} lg={4}>
+           <Card style={{ width: '100%' , height:'550px' }}>
+            <Card.Img variant="top" src={TeamImg4}  alt="Riaz" height={400}/>
+             <Card.Body>
+             <Card.Title className="title">RABIA RIAZ</Card.Title>
+               <Card.Text className="teaminfo">
+                <span className="tspan">HR & IT SPECIALIST</span><br/>
+                Experienced HR Manager from the Aviation Industry
+              </Card.Text>       
+             </Card.Body>
+             </Card>
+           </Col>
       </div>  
           {/* ====end container== */}
       </div>
